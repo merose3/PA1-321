930c92c7b10682d5758441944b4115a3a395c7e1
Mary Jane Rose <maryjane@Marys-MacBook-Pro-5.local>
2022-01-23 23:52:54 -0600

    Version 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PA1_321
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
